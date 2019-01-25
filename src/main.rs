a2be1ebd72d4f96b93e0700799925bdcbb3d4276
Simon Ellmann <kontakt@simonellmann.de>
2019-01-25 01:41:04 +0100

    Add wetter subdomain

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
-#![feature(proc_macro_hygiene, decl_macro)]
+#[macro_use]
+extern crate rocket;
 
-#[macro_use] extern crate rocket;
+use rocket::response::Redirect;
 
 #[get("/")]
 fn index() -> &'static str {
     "Hello, world!"
 }
 
-fn main() {
-    rocket::ignite().mount("/", routes![index]).launch();
+#[get("/wetter")]
+fn wetter() -> Redirect {
+    Redirect::to("https://www.wetter.de/cms/locker-20-grad-in-den-bergen-ueberall-strenger-frost-in-deutschland-4280453.html?c=bed2")
 }
 
+fn main() {
+    rocket::ignite()
+        .mount("/", routes![index, wetter])
+        .mount("/www", routes![index])
+        .launch();
+}
